34b74f7385c49b5005314d92e41d8cbe0bb0b48a
Arya <automic@Autumns-MacBook-Air.local>
2023-03-31 09:52:21 -0400

    app14- css grid

--- app14/src/components/B6.vue
+++ app14/src/components/B6.vue
@@ -13,20 +13,5 @@
   border-radius: 5px;
   text-align: center;
 }
-.parent {
-display: grid;
-grid-template-columns: repeat(5, 1fr);
-grid-template-rows: repeat(8, 1fr);
-grid-column-gap: 0px;
-grid-row-gap: 0px;
-}
-
-.div1 { grid-area: 1 / 1 / 2 / 6; }
-.div2 { grid-area: 8 / 1 / 9 / 6; }
-.div3 { grid-area: 5 / 4 / 8 / 6; }
-.div4 { grid-area: 5 / 1 / 8 / 4; }
-.div5 { grid-area: 2 / 1 / 5 / 3; }
-.div6 { grid-area: 2 / 3 / 5 / 6; }
-
 
 </style>
